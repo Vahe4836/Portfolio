92120d40fd8cfba04b7ca7dceb7b5b99f4f4914b
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2023-04-15 11:34:54 +0400

    adding some components

--- src/Main/Pages/Home/HomeProjects/HomeProjects.jsx
+++ src/Main/Pages/Home/HomeProjects/HomeProjects.jsx
@@ -2,10 +2,21 @@ import "./HomeProjects.scss";
 
 
 
-export default function HomeProjects(){
-    return(
-        <div>
-            HomeProjects
+export default function HomeProjects() {
+    return (
+        <div className="home_projects_div">
+            <h2 className="home_my_projects_title">My projects</h2>
+            <div className="home_projects_border">
+                <div className="home_project_auto_electric">
+                    <p className="home_project_text">Autoelectrickrd.ru</p>
+                </div>
+                <div className="home_project_spy_game">
+                   <p className="home_project_text">Spy Game</p>
+                </div>
+                <div className="home_project_other">
+                    <p className="home_project_text">+1</p>
+                </div>
+            </div>
         </div>
     )
 }
