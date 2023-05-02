33e60446225327d87a90a6a41e4de17a3fa55615
Vahe <vahehovakimyan574@gmail.com>
2023-05-03 00:17:02 +0400

    add icons

--- client/src/Main/Header/Header.jsx
+++ client/src/Main/Header/Header.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import github_icon from '../../images/home_icons/github_icon_header.png';
+import github_icon_test from '../../images/home_icons/Frame 4.png';
 import "./Header.scss";
 
 
@@ -7,7 +9,7 @@ export default function Header() {
         <div className="header">
             <div className="header_block">
                 <div className="github_link_block">
-                    <div className="github_icon"></div><a href="https://github.com/vahehovakimyan" className="github_link" target="_blank">My GitHub</a>
+                    <img src={github_icon_test} alt="github_icon" className="github_icon"/><a href="https://github.com/vahehovakimyan" className="github_link" target="_blank">My GitHub</a>
                 </div>
                 <nav>
 
