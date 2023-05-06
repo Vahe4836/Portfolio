bbe738c4b793c8e5ad008cb3a4e6ee8299e1e748
Vahe <vahehovakimyan574@gmail.com>
2023-05-07 00:01:57 +0400

    add footer icons

--- client/src/Main/Footer/Footer.jsx
+++ client/src/Main/Footer/Footer.jsx
@@ -1,7 +1,10 @@
-import linkedin_icon from '../../images/footer_icons/footer_linkedin_icon.png';
-import facebook_icon from '../../images/footer_icons/footer_facebook_icon.png';
-import instagram_icon from '../../images/footer_icons/footer_instagram_icon.png';
-import github_icon from '../../images/footer_icons/footer_github_icon.png';
+import linkedin_icon from '../../images/footer_icons/social/footer_linkedin_icon.png';
+import facebook_icon from '../../images/footer_icons/social/footer_facebook_icon.png';
+import instagram_icon from '../../images/footer_icons/social/footer_instagram_icon.png';
+import github_icon from '../../images/footer_icons/social/footer_github_icon.png';
+import mail_icon from '../../images/footer_icons/contact/mail_icon.png';
+import phone_icon from '../../images/footer_icons/contact/phone_icon.png';
+import address_icon from '../../images/footer_icons/contact/address_icon.png';
 import "./Footer.scss";
 
 
@@ -12,15 +15,15 @@ export default function Footer() {
             <div className="footer_div">
                 <div className="footer_contacts">
                     <div className="footer_contact_icon_text">
-                        <div className="footer_contacts_icon"></div>
+                        <img src={mail_icon} alt="mail_icon" width="20px" height="20px" />
                         <span className="footer_contacts_text"> Email: vahe.hovakimyan.19@mail.ru</span>
                     </div>
                     <div className="footer_contact_icon_text">
-                        <div className="footer_contacts_icon"></div>
+                        <img src={phone_icon} alt="phone_icon" width="20px" height="20px" />
                         <span className="footer_contacts_text">Telephone: (+374) 94 177 419</span>
                     </div>
                     <div className="footer_contact_icon_text">
-                        <div className="footer_contacts_icon"></div>
+                        <img src={address_icon} alt="address_icon" width="20px" height="20px" style={{border: "2px white solid", borderRadius: "50%"}} />
                         <span className="footer_contacts_text">Address: Yerevan, Armenia</span>
                     </div>
                 </div>
--- client/src/Main/Pages/Skills/Skills.jsx
+++ client/src/Main/Pages/Skills/Skills.jsx
@@ -37,7 +37,7 @@ export default function Skills() {
     return (
         <div className='skills_page'>
 
-            <section className='front_end_section'>
+            <section className='skills_section'>
                 <h2 className='skills_page_title'>Front End skills</h2>
 
                 <div className='skills_div_flex'>
@@ -64,10 +64,94 @@ export default function Skills() {
                         })}
                     </div>
                 </div>
-
             </section>
 
-            <h2 className='skills_page_title'>Back End skills</h2>
+
+
+            <section className='skills_section'>
+                <h2 className='skills_page_title'>Back End skills</h2>
+                <div className='skills_div_flex'>
+                    <div className='skills_div'>
+                        {backEndData.map((item) => {
+                            return (
+                                <div
+                                    className="skill_animation" style={{
+                                        background: `conic-gradient(${item.backgroundColor} ${item.percentage * 3.6}deg, rgba(255, 255, 255, 0) 0deg)`
+                                    }}
+                                    key={item.id}
+                                >
+
+                                    <div className="skill_percentage_animation">
+                                        <div className="skill_icon_text">
+                                            <p className="skills_text">{item.name}</p>
+                                            <img src={item.img} alt={item.alt} width="55px" height="55px" />
+                                            <p className="percentage_text">{item.percentage}%</p>
+                                        </div>
+                                        <div className="skill_opacity"></div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </section>
+
+            <section className='skills_section'>
+                <h2 className='skills_page_title'>Design skills</h2>
+                <div className='skills_div_flex'>
+                    <div className='skills_div'>
+                        {designData.map((item) => {
+                            return (
+                                <div
+                                    className="skill_animation" style={{
+                                        background: `conic-gradient(${item.backgroundColor} ${item.percentage * 3.6}deg, rgba(255, 255, 255, 0) 0deg)`
+                                    }}
+                                    key={item.id}
+                                >
+
+                                    <div className="skill_percentage_animation">
+                                        <div className="skill_icon_text">
+                                            <p className="skills_text">{item.name}</p>
+                                            <img src={item.img} alt={item.alt} width="55px" height="55px" />
+                                            <p className="percentage_text">{item.percentage}%</p>
+                                        </div>
+                                        <div className="skill_opacity"></div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </section>      
+
+
+            <section className='skills_section'>
+                <h2 className='skills_page_title'>Other skills</h2>
+                <div className='skills_div_flex'>
+                    <div className='skills_div'>
+                        {otherData.map((item) => {
+                            return (
+                                <div
+                                    className="skill_animation" style={{
+                                        background: `conic-gradient(${item.backgroundColor} ${item.percentage * 3.6}deg, rgba(255, 255, 255, 0) 0deg)`
+                                    }}
+                                    key={item.id}
+                                >
+
+                                    <div className="skill_percentage_animation">
+                                        <div className="skill_icon_text">
+                                            <p className="skills_text">{item.name}</p>
+                                            <img src={item.img} alt={item.alt} width="55px" height="55px" />
+                                            <p className="percentage_text">{item.percentage}%</p>
+                                        </div>
+                                        <div className="skill_opacity"></div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </section>
 
         </div>
 
