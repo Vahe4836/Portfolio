238dc0684272fbe0d9663433108c2f415a3000fe
Vahe <vahehovakimyan574@gmail.com>
2023-05-08 22:30:28 +0400

    create contact page

--- client/src/Main/Pages/Contact/Contact.jsx
+++ client/src/Main/Pages/Contact/Contact.jsx
@@ -13,12 +13,33 @@ export default function Contact() {
                 <div className='social_div'></div>
             </div>
 
-            <div className='form_div'>
-                <input type="text" placeholder='Name' className='input_style' />
-                <input type="text" placeholder='E-mail' className='input_style' />
-                <input type="text" placeholder='Subject' className='input_style' />
-                <textarea name="message" id="" cols="30" rows="10" placeholder='Message' className='textarea_style'></textarea>
+            <div className='form_div_flex'>
+                <div className='form_div'>
+
+                    <h1 className='contact_me_title'>Contact me</h1>
+
+                    <p className='contact_me_text'>
+                        I am interested in both office jobs and freelance opportunities.
+                        However, if you have any other query or question,
+                        don't hesitate to use the form.
+                    </p>
+
+                    <div className="name_Email_div">
+                        <input type="text" placeholder='Name' className='input_style' />
+                        <input type="text" placeholder='E-mail' className='input_style' />
+                    </div>
+
+                    <input type="text" placeholder='Subject' className='input_style_subject' />
+                    <textarea name="message" id="" cols="30" rows="10" placeholder='Message' className='textarea_style'></textarea>
+
+                    <div className='send_button_div'>
+                        <button className='send_button'>Send</button>
+                    </div>
+
+                </div>
             </div>
+
+
 
         </div>
     )
--- client/src/Main/Pages/Home/HomeProjects/HomeProjects.jsx
+++ client/src/Main/Pages/Home/HomeProjects/HomeProjects.jsx
@@ -8,21 +8,29 @@ export default function HomeProjects() {
 
         <div className="home_projects_div">
             <h2 className="home_my_projects_title">My projects</h2>
-            {/* <Link to="/projects" className="link_to_pages_from_home"> */}
             <div className="home_projects_border">
                 <Link to="/projects" className="link_to_pages_from_home">
+
                     <div className="home_project_auto_electric">
-                        <div className="home_project_opacity"><p className="home_project_text">Autoelectrickrd.ru</p></div>
+                        <div className="home_project_opacity">
+                            <p className="home_project_text">Autoelectrickrd.ru</p>
+                        </div>
                     </div>
+
                     <div className="home_project_spy_game">
-                        <div className="home_project_opacity"><p className="home_project_text">Spy Game</p></div>
+                        <div className="home_project_opacity">
+                            <p className="home_project_text">Spy Game</p>
+                        </div>
                     </div>
+
                     <div className="home_project_other">
-                        <div className="home_project_opacity"><p className="home_project_text">+2   ...</p></div>
+                        <div className="home_project_opacity">
+                            <p className="home_project_text">+2 ...</p>
+                            </div>
                     </div>
+
                 </Link>
             </div>
-            {/* </Link> */}
         </div>
     )
 }
