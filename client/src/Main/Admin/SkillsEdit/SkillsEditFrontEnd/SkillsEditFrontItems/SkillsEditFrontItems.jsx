3fde7edceb5e65ae3dae5f79b094acace237c7fb
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2023-07-23 22:32:40 +0400

    update admin page

--- client/src/Main/Admin/SkillsEdit/SkillsEditFrontEnd/SkillsEditFrontItems/SkillsEditFrontItems.jsx
+++ client/src/Main/Admin/SkillsEdit/SkillsEditFrontEnd/SkillsEditFrontItems/SkillsEditFrontItems.jsx
@@ -7,6 +7,8 @@ export default function SkillEditFrontItems({ frontEndData, setFrontEndData, onD
 
     const [displayFrontSkill, setDisplayFrontSkill] = useState(false);
 
+    const [allowEdit, setAllowEdit] = useState(false);
+
 
     return (
 
@@ -28,19 +30,40 @@ export default function SkillEditFrontItems({ frontEndData, setFrontEndData, onD
 
                 <h2 className='skills_page_title'>Front End skills</h2>
 
+                <div className="display_front_skill_button_div">
+                    <button
+                        className="display_front_skill_button"
+                        onClick={(evt) => {
+                            evt.preventDefault();
+                            setDisplayFrontSkill(!displayFrontSkill);
+                        }}>{displayFrontSkill ? "Close Front-End skills" : "Open Front-End skills"}</button>
+
+                </div>
+
+                <div className="check_button_div">
+                    <span className="check_button_text">{allowEdit ? "Open skills" : "Close skills"}</span>
+                    <input
+                        type="checkbox"
+                        name="hide"
+                        id="hide"
+                        value={allowEdit}
+                        onChange={() => { setAllowEdit(!allowEdit) }}
+                    />
+                </div>
+
+                <div className='skills_div_flex'
+                    style={{ display: `${displayFrontSkill ? "flex" : "none"}` }}
+                >
 
 
-
-
-                <div className='skills_div_flex' 
-                // style={{ display: `${displayFrontSkill ? "block" : "none"}` }}
-                 >
                     <div className='skills_div'>
                         {frontEndData.map((item) => {
                             return (
                                 <div
                                     className="skill_animation" style={{
-                                        background: `conic-gradient(${item.backgroundColor} ${item.percentage * 3.6}deg, rgba(255, 255, 255, 0) 0deg)`
+                                        background: `conic-gradient(${item.backgroundColor} ${item.percentage * 3.6}deg, rgba(255, 255, 255, 0) 0deg)`,
+                                        opacity: `${allowEdit ? "1" : "0.5"}`,
+                                        pointerEvents: `${allowEdit ? "auto" : "none"}`
                                     }}
                                     key={item.id}
                                 >
